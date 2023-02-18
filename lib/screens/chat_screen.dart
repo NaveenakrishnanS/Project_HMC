d12e93be2b05fb0039e7f536b4aa1f7248066528
Ganeshamurthy <jawaharrock666@gmail.com>
2023-02-18 14:45:53 +0530

    added chat screen and single chat screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class chatScreen extends StatefulWidget {
+  const chatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<chatScreen> createState() => _chatScreenState();
+}
+
+class _chatScreenState extends State<chatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/single_chat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class singleChat extends StatefulWidget {
+  const singleChat({Key? key}) : super(key: key);
+
+  @override
+  State<singleChat> createState() => _singleChatState();
+}
+
+class _singleChatState extends State<singleChat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
