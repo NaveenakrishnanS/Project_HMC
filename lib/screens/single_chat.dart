0c97dfd7810b94ea1d1db1cfd5dfd3133c7cfe06
S.Naveenakrishnan <infinitetrex0077@gmail.com>
2023-04-24 15:42:34 +0530

    Update single_chat.dart

--- lib/screens/single_chat.dart
+++ lib/screens/single_chat.dart
@@ -3,15 +3,51 @@ import 'package:project_hmc/screens/message_card/receive_card.dart';
 import 'package:project_hmc/screens/message_card/send_card.dart';
 
 class SingleChat extends StatefulWidget {
-  const SingleChat({Key? key, required this.name,required this.uID}) : super(key: key);
+  const SingleChat({Key? key, required this.name, required this.uID})
+      : super(key: key);
   final String name, uID;
+
   @override
   State<SingleChat> createState() => _SingleChatState();
 }
 
 class _SingleChatState extends State<SingleChat> {
   final TextEditingController _controller = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
 
+  final List _messages = [
+    const InputMessage(text: 'Hii'),
+    const ReplyMessage(text: 'Hi'),
+    const InputMessage(text: 'How are you?'),
+    const ReplyMessage(text: 'Fine'),
+    const ReplyMessage(text: 'How about you?'),
+    const InputMessage(text: 'Great Life'),
+    const ReplyMessage(text: 'Enjoying developer\'s Life'),
+    const InputMessage(text: 'Yeah'),
+    const InputMessage(text: 'How is it being Entrepreneur?'),
+    const ReplyMessage(text: 'yeah, Going Great'),
+    const InputMessage(text: 'Send me the pics'),
+    const ReplyMessage(text: 'Fine'),
+    const ReplyMessage(text: 'do you need all?'),
+    const InputMessage(text: 'Yeah, man'),
+    const ReplyMessage(text: 'ok Cool!'),
+    const InputMessage(text: 'Hmm'),
+    const ReplyMessage(text: 'Mmmm'),
+    const InputMessage(text: 'Check Your Mail'),
+    const ReplyMessage(text: 'Wait a second! Let me check it'),
+    const ReplyMessage(text: 'Ok! All looks great'),
+    const InputMessage(text: 'I too felt it same as you are'),
+    const ReplyMessage(text: 'Ok! Bye! Got to go!'),
+    const InputMessage(text: 'Bye! Bye!'),
+  ];
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Scroll to the end of the list on page load
+      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -21,140 +57,127 @@ class _SingleChatState extends State<SingleChat> {
          width: MediaQuery.of(context).size.width,
          fit: BoxFit.cover,),*/
         Scaffold(
-          backgroundColor: Colors.white,
-          appBar: PreferredSize(
-            preferredSize: const Size.fromHeight(65),
-            child: AppBar(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(0)),
-              automaticallyImplyLeading: true,
-              leadingWidth: 350,
-              elevation: 0,
-              backgroundColor: Colors.black,
-              leading: Align(
-                child: Padding(
-                    padding: const EdgeInsets.only(top: 15, left: 10),
-                    child: Row(
-                      children:  [
-                        CircleAvatar(
-                          child: Image.asset(
-                            'assets/user.jpg'
+            backgroundColor: Colors.white,
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(65),
+              child: AppBar(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0)),
+                automaticallyImplyLeading: true,
+                leadingWidth: 350,
+                elevation: 0,
+                backgroundColor: Colors.black,
+                leading: Align(
+                  child: Padding(
+                      padding: const EdgeInsets.only(top: 15, left: 10),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            child: Image.asset('assets/user.jpg'),
                           ),
-                        ),
-                        // const Icon(
-                        //   Icons.person,
-                        //   color: Colors.amber,
-                        //   weight: Checkbox.width,
-                        //   size: 40,
-                        // ),
-                        const SizedBox(
-                          width: 7,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: Text(
-                            widget.name,
-                            style: const TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
+                          // const Icon(
+                          //   Icons.person,
+                          //   color: Colors.amber,
+                          //   weight: Checkbox.width,
+                          //   size: 40,
+                          // ),
+                          const SizedBox(
+                            width: 7,
                           ),
-                        ),
-                      ],
-                    )),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              widget.name,
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      )),
+                ),
               ),
             ),
-          ),
-          resizeToAvoidBottomInset: true,
-          body: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.height,
-            child: Stack(
-              children: [
-                Container(
-                  color: Colors.white,
-                  height: MediaQuery.of(context).size.height - 160,
-                  child: ListView(
-                    padding: const EdgeInsets.only(bottom: 15),
-                    shrinkWrap: true,
-                    children: const [
-                      InputMessage(text: 'Hii'),
-                      ReplyMessage(text: 'Hi'),
-                      InputMessage(text: 'How are you?'),
-                      ReplyMessage(text: 'Fine'),
-                      ReplyMessage(text: 'How about you?'),
-                      InputMessage(text: 'Great Life'),
-                      ReplyMessage(text: 'Enjoying developer\'s Life'),
-                      InputMessage(text: 'Yeah'),
-                      InputMessage(text: 'How is it being Entrepreneur?'),
-                      ReplyMessage(text: 'yeah, Going Great'),
-                      InputMessage(text: 'Send me the pics'),
-                      ReplyMessage(text: 'Fine'),
-                      ReplyMessage(text: 'do you need all?'),
-                      InputMessage(text: 'Yeah, man'),
-                      ReplyMessage(text: 'ok Cool!'),
-                      InputMessage(text: 'Hmm'),
-                      ReplyMessage(text: 'Mmmm'),
-                      InputMessage(text: 'Check Your Mail'),
-                      ReplyMessage(text: 'Wait a second! Let me check it'),
-                      ReplyMessage(text: 'Ok! All looks great'),
-                      InputMessage(text: 'I too felt it same as you are'),
-                      ReplyMessage(text: 'Ok! Bye! Got to go!'),
-                      InputMessage(text: 'Bye! Bye!'),
-                    ],
+            resizeToAvoidBottomInset: true,
+            body: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 0),
+                    decoration: const BoxDecoration(color: Colors.white),
+                    child: SingleChildScrollView(
+                      child: ListView.builder(
+                        controller: _scrollController,
+                        padding: const EdgeInsets.only(bottom: 80),
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: _messages.length,
+                        itemBuilder: (context, index) {
+                          return _messages[index];
+                        },
+                      ),
+                    ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 2.0, horizontal: 2.0),
-                          child: TextField(
-                            controller: _controller,
-                            style: const TextStyle(
-                                fontSize: 20.0, color: Colors.black87),
-                            decoration: InputDecoration(
-                              hintText: 'Type a message',
-                              suffixIcon: IconButton(
-                                splashRadius: 10,
-                                icon: const Icon(Icons.attach_file_rounded),
-                                color: Colors.grey,
-                                onPressed: () {},
-                              ),
-                              filled: true,
-                              fillColor: Colors.white,
-                              contentPadding: const EdgeInsets.only(
-                                  left: 20.0,
-                                  bottom: 8.0,
-                                  top: 8.0,
-                                  right: 16.0),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(50.0),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          top: BorderSide(color: Colors.transparent, width: 1),
+                        ),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 10, bottom: 8),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.grey.shade300),
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                                  child: TextField(
+                                    controller: _controller,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Type your message...',
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ),
-                      Material(
-                        color: Colors.black,borderRadius: BorderRadius.circular(30),
-                        child: IconButton(
-                          splashRadius: 20,
-                          onPressed: () {},
-                          splashColor: Colors.black87,hoverColor: Colors.black87,
-                          icon: const Icon(
-                            Icons.send,
-                            color: Colors.white,
+                          IconButton(
+                            icon: const Icon(Icons.send),
+                            onPressed: () {
+                              InputMessage input =
+                              InputMessage(text: _controller.text);
+                              setState(() {
+                                _messages.add(input);
+                              });
+                              _controller.clear();
+                              _scrollController.animateTo(
+                                _scrollController.position.maxScrollExtent,
+                                duration:  const Duration(milliseconds: 500),
+                                curve: Curves.fastOutSlowIn,
+                              );
+                              FocusScope.of(context).unfocus();
+                            },
+                            color: Theme.of(context).primaryColor,
                           ),
-                          iconSize: 28,
-                        ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
         ),
       ],
     );
